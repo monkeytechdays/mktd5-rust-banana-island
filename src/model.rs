283c66d466117f9aef07e7a3269200c1a4b18d50
Logan Mzz <loganmzz@users.noreply.github.com>
2017-11-25 00:30:51 +0100

    FIX #1 Using Entity for cell content

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex};
+use serde::{Deserialize, Deserializer};
 
 #[derive(Default, Debug)]
 pub struct GameState {
@@ -29,20 +30,21 @@ pub type GameStateMutex = Arc<Mutex<GameState>>;
 #[derive(Deserialize, Debug)]
 pub struct Map {
     #[serde(rename="map")]
-    pub content: Vec<Vec<i64>>,
+    pub content: Vec<Vec<Entity>>,
 }
 impl Map {
     pub fn apply_move(&mut self, player_move: &PlayerMove) {
         if player_move.direction == Move::None {
             return;
         }
+        let player = Entity::Player(player_move.id);
 
         let mut indexes: Option<((usize, usize),(usize, usize))> = None;
         'global: for row_index in 0..self.content.len() {
             let row = &self.content[row_index];
             for col_index in 0..row.len() {
                 let val = row[col_index];
-                if val == player_move.id {
+                if val == player {
                     if let Some((target_row_index, target_col_index)) = player_move.direction.move_from(row_index, col_index) {
 
                         if target_row_index < self.content.len() {
@@ -59,8 +61,8 @@ impl Map {
         }
 
         if let Some(((src_row, src_col),(tgt_row, tgt_col))) = indexes {
-            self.content[src_row][src_col] = 0;
-            self.content[tgt_row][tgt_col] = player_move.id;
+            self.content[src_row][src_col] = Entity::Empty;
+            self.content[tgt_row][tgt_col] = player;
         }
     }
 }
@@ -68,82 +70,108 @@ impl Map {
 #[cfg(test)]
 mod map_should {
 
-    use super::{Map, PlayerMove, Move};
+    use super::{Map, PlayerMove, Move, Entity};
+    use super::Entity::*;
     use serde_json;
+
+    fn content(matrix: [[Entity; 3]; 3]) -> Vec<Vec<Entity>> {
+        matrix.into_iter()
+               .map(|row| row.to_vec())
+               .collect()
+    }
+
+    fn assert_content(expected: [[Entity; 3]; 3], map: Map) {
+        assert_eq!(content(expected), map.content);
+    }
+
+    fn new_map(matrix: [[Entity; 3]; 3]) -> Map {
+        let content = content(matrix);
+        Map { content }
+    }
 
     #[test]
     fn deserialize() {
         let data = r#"{
             "map": [
-                [1, 2, 3, 4],
-                [5, 6, 7, 8]
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
             ]
         }"#;
         let map: super::Map = serde_json::from_str(data).expect("Can't parse map resource");
-        assert_eq!(vec![vec![1, 2, 3, 4], vec![5, 6, 7, 8]], map.content);
+        assert_content([[Banana, Wall, Player(3)], [Player(4), Player(5), Player(6)], [Player(7), Player(8), Player(9)]], map);
     }
 
     #[test]
     fn do_nothing_when_applying_move_None() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]] };
+        let mut map = new_map([
+            [Empty, Empty    , Empty],
+            [Empty, Player(3), Empty],
+            [Empty, Empty    , Empty]
+        ]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::None });
-        assert_eq!(vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]], map.content);
+        assert_content(
+            [
+                [Empty, Empty    , Empty],
+                [Empty, Player(3), Empty],
+                [Empty, Empty    , Empty]
+            ], map);
     }
 
     #[test]
     fn move_up_when_applying_move_North() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Player(3), Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::North });
-        assert_eq!(vec![vec![0, 3, 0], vec![0, 0, 0], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Player(3), Empty], [Empty, Empty, Empty], [Empty, Empty, Empty]], map);
     }
 
     #[test]
     fn move_left_when_applying_move_West() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Player(3), Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::West });
-        assert_eq!(vec![vec![0, 0, 0], vec![3, 0, 0], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Player(3), Empty, Empty], [Empty, Empty, Empty]], map);
     }
 
     #[test]
     fn move_down_when_applying_move_South() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Player(3), Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::South });
-        assert_eq!(vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 3, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Empty, Empty, Empty], [Empty, Player(3), Empty]], map);
     }
 
     #[test]
     fn move_right_when_applying_move_East() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 3, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Player(3), Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::East });
-        assert_eq!(vec![vec![0, 0, 0], vec![0, 0, 3], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Empty, Empty, Player(3)], [Empty, Empty, Empty]], map);
     }
 
     #[test]
     fn do_nothing_when_applying_move_North_on_north_border() {
-        let mut map = Map { content: vec![vec![0, 3, 0], vec![0, 0, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Player(3), Empty], [Empty, Empty, Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::North });
-        assert_eq!(vec![vec![0, 3, 0], vec![0, 0, 0], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Player(3), Empty], [Empty, Empty, Empty], [Empty, Empty, Empty]], map);
     }
 
     #[test]
     fn do_nothing_when_applying_move_West_on_west_border() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![3, 0, 0], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Player(3), Empty, Empty], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::West });
-        assert_eq!(vec![vec![0, 0, 0], vec![3, 0, 0], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Player(3), Empty, Empty], [Empty, Empty, Empty]], map);
     }
 
     #[test]
     fn do_nothing_when_applying_move_South_on_south_border() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 3, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Empty, Empty], [Empty, Player(3), Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::South });
-        assert_eq!(vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 3, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Empty, Empty, Empty], [Empty, Player(3), Empty]], map);
     }
 
     #[test]
     fn do_nothing_when_applying_move_East_on_east_border() {
-        let mut map = Map { content: vec![vec![0, 0, 0], vec![0, 0, 3], vec![0, 0, 0]] };
+        let mut map = new_map([[Empty, Empty, Empty], [Empty, Empty, Player(3)], [Empty, Empty, Empty]]);
         map.apply_move(&PlayerMove { id: 3, direction: Move::East });
-        assert_eq!(vec![vec![0, 0, 0], vec![0, 0, 3], vec![0, 0, 0]], map.content);
+        assert_content([[Empty, Empty, Empty], [Empty, Empty, Player(3)], [Empty, Empty, Empty]], map);
     }
 }
 
@@ -224,5 +252,62 @@ mod game_tick_should {
             "moves": [{"id": 314, "move": "O"}]
         }"#;
         assert_eq!(GameTick { id: 42, moves: Some(vec![PlayerMove { id: 314, direction: Move::None}]) }, parse(data));
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Entity {
+    Empty,
+    Banana,
+    Wall,
+    Player(i64),
+}
+
+impl Entity {
+    pub fn from_i64(val: i64) -> Entity {
+        match val {
+            0  => Entity::Empty,
+            1  => Entity::Banana,
+            2  => Entity::Wall,
+            id => Entity::Player(id),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for Entity {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        let val = i64::deserialize(deserializer)?;
+        Ok(Entity::from_i64(val))
+    }
+}
+
+#[cfg(test)]
+mod entity_should {
+
+    use super::Entity;
+
+    fn deserialize_as(expected: Entity, from: &'static str) {
+        let entity: Entity = ::serde_json::from_str(from).unwrap();
+        assert_eq!(expected, entity);
+    }
+
+    #[test]
+    fn deserialize_from_0_as_empty() {
+        deserialize_as(Entity::Empty, "0");
+    }
+
+    #[test]
+    fn deserialize_from_1_as_banana() {
+        deserialize_as(Entity::Banana, "1");
+    }
+
+    #[test]
+    fn deserialize_from_2_as_wall() {
+        deserialize_as(Entity::Wall, "2");
+    }
+
+    #[test]
+    fn deserialize_from_3_as_player_3() {
+        deserialize_as(Entity::Player(3), "3");
     }
 }
